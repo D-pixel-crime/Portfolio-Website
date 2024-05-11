0b22479ecc163b043b5ad9e3d88fac989235f628
D-pixel-crime <dpk4383@gmail.com>
2024-05-12 00:24:12 +0530

    projects animation

--- src/slices/Projects/index.tsx
+++ src/slices/Projects/index.tsx
@@ -1,12 +1,17 @@
+"use client";
+
 import CommonBounded from "@/components/CommonBounded";
 import Heading from "@/components/Heading";
-import ProjectList from "@/components/ProjectList";
 import { Content } from "@prismicio/client";
+import { PrismicNextLink } from "@prismicio/next";
 import {
+  PrismicImage,
   PrismicRichText,
-  PrismicRichTextProps,
   SliceComponentProps,
 } from "@prismicio/react";
+import { SquareArrowOutUpRight, SquareArrowUpRight } from "lucide-react";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 
 /**
  * Props for `Projects`.
@@ -17,18 +22,75 @@ export type ProjectsProps = SliceComponentProps<Content.ProjectsSlice>;
  * Component for "Projects" Slices.
  */
 const Projects = ({ slice }: ProjectsProps): JSX.Element => {
+  const eachProjectRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    gsap.utils.toArray(".each-project").forEach((project, index) => {
+      gsap.fromTo(
+        project,
+        {
+          opacity: 0,
+          x: index % 2 === 0 ? 200 : -200,
+        },
+        {
+          opacity: 1,
+          x: 0,
+          duration: 2,
+          ease: "power3.inOut",
+        }
+      );
+    });
+  }, []);
+
   return (
     <CommonBounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <Heading size="xl" className="mb-8">
-        {slice.primary.heading}
-      </Heading>
-      <div className="prose prose-xl prose-invert mb-10">
-        <PrismicRichText field={slice.primary.description} />
+      <div className="grid grid-cols-[2fr,1fr] items-center mb-20">
+        <div className="col-start-1">
+          <Heading size="xl" className="mb-8">
+            {slice.primary.heading}
+          </Heading>
+          <div className="prose prose-xl prose-invert mb-10">
+            <PrismicRichText field={slice.primary.description} />
+          </div>
+        </div>
+        <PrismicImage
+          field={slice.primary.fallback_item_image}
+          alt=""
+          className="col-start-2 rounded-xl opacity-50"
+        />
       </div>
-      <ProjectList />
+      <div className="overflow-hidden hover:overflow-visible">
+        {slice.items.map(
+          ({ project_title, project_description, project_link }, index) => {
+            return (
+              <div
+                className="each-project border-y border-slate-600 opacity-0"
+                ref={eachProjectRef}
+                key={index}
+              >
+                <div className="flex flex-row items-center justify-between py-7 px-2 hover:bg-slate-800 hover:scale-105 transition">
+                  <div className="flex flex-col gap-3">
+                    <Heading size="sm">{project_title}</Heading>
+                    <div className="prose prose-lg prose-invert text-slate-400">
+                      <PrismicRichText field={project_description} />
+                    </div>
+                  </div>
+                  <PrismicNextLink
+                    field={project_link}
+                    className="flex-center gap-2 hover:scale-105 transition hover:text-yellow-400"
+                  >
+                    {slice.primary.view_project}
+                    <SquareArrowOutUpRight />
+                  </PrismicNextLink>
+                </div>
+              </div>
+            );
+          }
+        )}
+      </div>
     </CommonBounded>
   );
 };
