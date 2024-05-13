977155168c534f717d39a2620b7eb5a6dd749890
D-pixel-crime <dpk4383@gmail.com>
2024-05-13 17:29:58 +0530

    minor changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,19 +8,21 @@ const Header = async () => {
   const commonSettings = await client.getSingle("common_settings");
 
   return (
-    <header className="top-0 z-50 mx-auto max-w-7xl md:sticky">
-      <nav>
-        <ul>
+    <header className="top-0 z-50 mx-auto max-w-7xl md:sticky mb-16 mt-5 text-xl">
+      <nav className="mx-5">
+        <ul className="flex justify-between">
           <li>
             <Link href="/" aria-label="Home Page">
               {commonSettings.data.name}
             </Link>
           </li>
-          {commonSettings.data.nav_item.map(({ label, link }, index) => (
-            <li key={index}>
-              <PrismicNextLink field={link}>{label}</PrismicNextLink>
-            </li>
-          ))}
+          <div className="flex gap-5">
+            {commonSettings.data.nav_item.map(({ label, link }, index) => (
+              <li key={index}>
+                <PrismicNextLink field={link}>{label}</PrismicNextLink>
+              </li>
+            ))}
+          </div>
         </ul>
       </nav>
     </header>
